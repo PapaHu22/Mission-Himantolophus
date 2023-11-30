0462ab24fa9cd3d43703973728b1eb511c7ea0b1
0780prog <0780prog@gmail.com>
2023-11-30 15:59:07 +0900

    Squashed commit of the following:
    commit a37d3f744c774eb65dd1475413c54121f9a06003
    Author: dureamPark <doo7172@naver.com>
    Date:   Thu Nov 30 15:42:47 2023 +0900
    
        now
    
    commit d96e5efe62b7e0491214bce4c0db66e07160e652
    Author: dureamPark <doo7172@naver.com>
    Date:   Thu Nov 30 15:42:33 2023 +0900
    
        now
    
    commit 08f5474b06e5a6c5e1c3652d7b7a7c012201d601
    Author: dureamPark <doo7172@naver.com>
    Date:   Thu Nov 9 17:18:55 2023 +0900
    
        Homepage_link

--- Homepage/main.js
+++
@@ -1,8 +0,0 @@
-const togglebutton=document.querySelector(".anything_togglemenubarbutton");
-const menu=document.querySelector('.anything_menu');
-const icons=document.querySelector('.anything_icons');
-
-togglebutton.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
